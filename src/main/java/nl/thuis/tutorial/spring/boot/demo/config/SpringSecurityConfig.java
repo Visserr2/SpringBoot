379d8116d4cc71fea023c3476f8fef6991b7800d
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-05-27 20:17:58 +0200

    Configured Spring Security, Spring Actuator and Spring Mvc

---
+++ src/main/java/nl/thuis/tutorial/spring/boot/demo/config/SpringSecurityConfig.java
@@ -0,0 +1,20 @@
+package nl.thuis.tutorial.spring.boot.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers("/actuator/**").authenticated()
+			.antMatchers("/**").permitAll()
+		.and()
+			.formLogin();
+	}
+
+}
